8a35852ea9f591ede3189199f4f8f61ebbc2da35
avalon <asd95027@gmail.com>
2023-05-18 21:36:50 +0800

    蒐集靜態檔案+whitenoise

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'todolist.urls'
@@ -88,6 +90,7 @@ DATABASES = {
     'NAME': 'todolist',
     'USER': 'avalon',
     'PASSWORD': '12345678',
+    #'HOST': 'localhost',#127.0.0.1
     'HOST': 'database-2.c0y3lgknejqp.ap-northeast-1.rds.amazonaws.com',#127.0.0.1
     'PORT': '3306',
     }
@@ -128,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
